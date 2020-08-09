12f116e86266cd48811dfaee4c93cac7f5e6c51c
djonnyx <djonnyx@gmail.com>
2020-08-09 23:43:06 +0300

    hotfix: order-types ref create init table

--- src/db/initDB.ts
+++ src/db/initDB.ts
@@ -59,6 +59,10 @@ export const initRefs = async (): Promise<void> => {
             name: RefTypes.CURRENCIES,
             version: 1,
             lastUpdate,
+        }, {
+            name: RefTypes.ORDER_TYPES,
+            version: 1,
+            lastUpdate,
         },
     ];
 
