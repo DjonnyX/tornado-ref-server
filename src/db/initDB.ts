a7310485a235f04706cf671e6f5324f4125c8a0d
djonnyx <djonnyx@gmail.com>
2020-08-10 09:46:36 +0300

    hotfix: translation table add to init db

--- src/db/initDB.ts
+++ src/db/initDB.ts
@@ -71,6 +71,10 @@ export const initRefs = async (): Promise<void> => {
             name: RefTypes.ORDER_TYPES,
             version: 1,
             lastUpdate,
+        }, {
+            name: RefTypes.TRANSLATION,
+            version: 1,
+            lastUpdate,
         },
     ];
 
