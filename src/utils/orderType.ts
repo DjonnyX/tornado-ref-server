533f0355dce9f4cd2de06b43f552480db2850815
djonnyx <djonnyx@gmail.com>
2020-08-26 15:14:56 +0300

    feat: order type multi content implemented

--- src/utils/orderType.ts
+++ src/utils/orderType.ts
@@ -3,13 +3,6 @@ import { IOrderType } from "@models";
 export const formatOrderTypeModel = (model: IOrderType) => ({
     id: model._id,
     active: model.active,
-    name: model.name,
-    description: model.description,
-    color: model.color,
-    assets: model.assets,
-    images: model.images || {
-        main: null,
-        icon: null,
-    },
+    contents: model.contents,
     extra: model.extra,
 });
