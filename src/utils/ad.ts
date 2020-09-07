62576c589a9eb450cb632ac45ecabc81bcc109ea
djonnyx <djonnyx@gmail.com>
2020-09-07 13:26:54 +0300

    feat: ad controller improved

--- src/utils/ad.ts
+++ src/utils/ad.ts
@@ -1,7 +1,10 @@
 import { IAd } from "@models";
+import { IAdItem } from "../controllers/AdController";
 
-export const formatAdModel = (model: IAd) => ({
+export const formatAdModel = (model: IAd): IAdItem => ({
     id: model._id,
+    name: model.name,
+    type: model.type,
     active: model.active,
     contents: model.contents,
     extra: model.extra,
