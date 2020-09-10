7faa69a36d23db86f2eb6593380d99d380367960
djonnyx <djonnyx@gmail.com>
2020-09-10 15:37:23 +0300

    hotfix: added address to the store schema

--- src/models/Store.ts
+++ src/models/Store.ts
@@ -14,7 +14,8 @@ interface IStoreDocument extends Document, IStore {}
 
 const AdSchema = new Schema({
     active: { type: Boolean, required: true, default: true },
-    name: { type: String, required: false },
+    name: { type: String, required: true },
+    address: { type: String, required: false },
     terminals: [{ type: Schema.Types.ObjectId }],
     employes: [{ type: Schema.Types.ObjectId }],
     extra: { type: Schema.Types.Mixed, required: false, default: {} },
