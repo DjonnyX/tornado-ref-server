2b52d2c15efd2848c9e020887ca4f3f3c8071597
djonnyx <djonnyx@gmail.com>
2020-10-07 19:49:12 +0300

    feat: returned to http server from https

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 import * as https from "https";
+import * as http from "http";
 import * as fs from "fs";
 
 import * as mongoose from "mongoose";
@@ -14,7 +15,7 @@ const httpsOptions = {
     cert: fs.readFileSync("keystore/cert.pem")
 };
 
-const server = https.createServer(httpsOptions, app);
+const server = http.createServer(app); //https.createServer(httpsOptions, app);
 server.listen(PORT);
 server.on("listening", () => {
     console.info(`Listening on port ${PORT}.`);
