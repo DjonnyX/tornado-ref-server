6fa462672f9527577b15009c5bd7cd4679d20df1
djonnyx <djonnyx@gmail.com>
2021-02-05 13:16:26 +0300

    feat: findAllWithFilter improved

--- src/utils/requestOptions.ts
+++ src/utils/requestOptions.ts
@@ -1,7 +1,7 @@
 import { DocumentQuery } from "mongoose";
 import { IAuthRequest } from "../interfaces";
 
-const FILTER_PATTERN = /([\w.])*\.\w{1,}$/;
+const FILTER_PATTERN = /([\w.])*\.(equals|contain|notequals|lt|gt){1,}$/;
 
 export function findAllWithFilter<T, D, Q>(document: DocumentQuery<T, any, Q>, request: IAuthRequest): DocumentQuery<T, any, Q> {
     let result = document;
@@ -12,10 +12,16 @@ export function findAllWithFilter<T, D, Q>(document: DocumentQuery<T, any, Q>, r
             const operation = pName.substr(pName.lastIndexOf('.') + 1);
             const value = request.query[pName];
 
-            if (operation === 'INCLUDE') {
+            if (operation === 'equals') {
                 result = result.where(id).equals(value);
-            } else if (operation === 'EXCLUDE') {
+            } else if (operation === 'contain') {
                 // etc
+            } else if (operation === 'notequals') {
+                // etc
+            } else if (operation === 'lt') {
+                result = result.where(id).lt(value);
+            } else if (operation === 'gt') {
+                result = result.where(id).gt(value);
             }
         }
     }
