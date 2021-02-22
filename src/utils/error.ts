de012d79281248c333f561eea33a76477bf9172f
djonnyx <djonnyx@gmail.com>
2021-02-22 09:25:49 +0300

    feat: #81 error handling improved

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,4 +1,4 @@
-export const extractError = (error: Array<{code: number | string, message: string}>): string | undefined => {
+export const extractError = (error: Array<{ code: number | string, message: string }>): string | undefined => {
     if (!!error && error.length > 0) {
         let err = "";
 
@@ -10,4 +10,17 @@ export const extractError = (error: Array<{code: number | string, message: strin
     }
 
     return undefined;
+}
+
+const ERR_PATTERN = /(Error: )([\w]*)/gm;
+
+export const extract401Error = (err: string) => {
+    if (!!err) {
+        const s = err.match(ERR_PATTERN);
+        if (!!s && s.length > 0) {
+            return s[0].replace(/Error: /g, "");
+        }
+    }
+
+    return "Unknown error.";
 }
