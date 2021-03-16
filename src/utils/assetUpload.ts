ed0e3c43f41debf84566a81802631436c111ca03
djonnyx <djonnyx@gmail.com>
2021-03-16 15:54:39 +0300

    feat: #78 decode video to h264 implemented

--- src/utils/assetUpload.ts
+++ src/utils/assetUpload.ts
@@ -27,7 +27,35 @@ const THUMBNAIL_HEIGHT = 128;
 const THUMBNAIL_FAV_WIDTH = 32;
 const THUMBNAIL_FAV_HEIGHT = 32;
 
-export const makeThumbnail = (ext: string, pathToResource: string, width: number, height: number): Promise<string> => {
+const decodeVideo = (ext: string, pathToResource: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const normalizedPathToResource = `${pathToResource}${ext}`;
+        const normalizedTmpPathToResource = `${pathToResource}-tmp${ext}`;
+        try {
+            ffmpeg(normalizedPathToResource)
+                .outputOptions("-c:v libx264")
+                .size("1920x?")
+                .noAudio()
+                .on('error', (err) => {
+                    reject(err);
+                })
+                .on('end', () => {
+                    try {
+                        fs.unlinkSync(normalizedPathToResource);
+                        fs.renameSync(normalizedTmpPathToResource, normalizedPathToResource);
+                    } catch (err) {
+                        return reject(err);
+                    }
+                    resolve(normalizedPathToResource);
+                })
+                .save(normalizedTmpPathToResource)
+        } catch (err) {
+            return reject(err);
+        }
+    });
+}
+
+const makeThumbnail = (ext: string, pathToResource: string, width: number, height: number): Promise<string> => {
     return new Promise((resolve, reject) => {
         const thumbnailPath = path.normalize(`${pathToResource}_${width}x${height}`);
         const normalizedPathToResource = `${pathToResource}${ext}`
@@ -35,30 +63,38 @@ export const makeThumbnail = (ext: string, pathToResource: string, width: number
         if (ext === AssetExtensions.JPG || ext === AssetExtensions.PNG) {
             const normalizedThumbnailPath = `${thumbnailPath}${ext}`
             // создается миниатюра
-            sharp(normalizedPathToResource)
-                .ensureAlpha()
-                .resize(width, height, {
-                    fit: "contain",
-                    background: { r: 0, g: 0, b: 0, alpha: 0 },
-                })
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                .toFile(normalizedThumbnailPath, (err, info) => {
-                    if (!!err) {
-                        return reject(Error(`Thumbnail is not created. ${err}`));
-                    };
-                    resolve(normalizedThumbnailPath);
-                });
+            try {
+                sharp(normalizedPathToResource)
+                    .ensureAlpha()
+                    .resize(width, height, {
+                        fit: "contain",
+                        background: { r: 0, g: 0, b: 0, alpha: 0 },
+                    })
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    .toFile(normalizedThumbnailPath, (err, info) => {
+                        if (!!err) {
+                            return reject(Error(`Thumbnail is not created. ${err}`));
+                        };
+                        resolve(normalizedThumbnailPath);
+                    });
+            } catch (err) {
+                return reject(err);
+            }
         } else if (ext === AssetExtensions.MP4) {
-            ffmpeg(normalizedPathToResource)
-                .size(`${width}x${height}`)
-                .outputOption("-vf", `scale=${width}:-1:flags=lanczos,fps=8`)
-                .on('error', (err) => {
-                    reject(err);
-                })
-                .on('end', () => {
-                    resolve(`${thumbnailPath}.gif`);
-                })
-                .save(`${thumbnailPath}.gif`)
+            try {
+                ffmpeg(normalizedPathToResource)
+                    .size(`${width}x${height}`)
+                    .outputOption("-vf", `scale=${width}:-1:flags=lanczos,fps=8`)
+                    .on('error', (err) => {
+                        return reject(err);
+                    })
+                    .on('end', () => {
+                        resolve(`${thumbnailPath}.gif`);
+                    })
+                    .save(`${thumbnailPath}.gif`)
+            } catch (err) {
+                return reject(err);
+            }
         }
     });
 };
@@ -82,23 +118,48 @@ export const assetsUploader = (name: string, allowedExtensions: Array<AssetExten
 
             const ext = path.extname(request.file.originalname) as AssetExtensions;
             const filePath = `${request.file.path}${ext}`;
-            fs.renameSync(request.file.path, filePath);
 
-            makeThumbnail(ext, request.file.path, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT).then(x128Path => {
-                makeThumbnail(ext, request.file.path, THUMBNAIL_FAV_WIDTH, THUMBNAIL_FAV_HEIGHT).then(x32Path => {
-                    resolve({
-                        active: true,
-                        name: request.file.originalname,
-                        lastUpdate: Date.now(),
-                        ext,
-                        mipmap: {
-                            x128: x128Path,
-                            x32: x32Path,
-                        },
-                        path: filePath,
+            try {
+                fs.renameSync(request.file.path, filePath);
+            } catch (err) {
+                return reject(err);
+            }
+
+            if (ext === AssetExtensions.MP4) {
+                decodeVideo(ext, request.file.path).then(filePath => {
+                    makeThumbnail(ext, request.file.path, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT).then(x128Path => {
+                        makeThumbnail(ext, request.file.path, THUMBNAIL_FAV_WIDTH, THUMBNAIL_FAV_HEIGHT).then(x32Path => {
+                            resolve({
+                                active: true,
+                                name: request.file.originalname,
+                                lastUpdate: Date.now(),
+                                ext,
+                                mipmap: {
+                                    x128: x128Path,
+                                    x32: x32Path,
+                                },
+                                path: filePath,
+                            });
+                        });
                     });
                 });
-            });
+            } else {
+                makeThumbnail(ext, request.file.path, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT).then(x128Path => {
+                    makeThumbnail(ext, request.file.path, THUMBNAIL_FAV_WIDTH, THUMBNAIL_FAV_HEIGHT).then(x32Path => {
+                        resolve({
+                            active: true,
+                            name: request.file.originalname,
+                            lastUpdate: Date.now(),
+                            ext,
+                            mipmap: {
+                                x128: x128Path,
+                                x32: x32Path,
+                            },
+                            path: filePath,
+                        });
+                    });
+                });
+            }
         });
     });
 };
